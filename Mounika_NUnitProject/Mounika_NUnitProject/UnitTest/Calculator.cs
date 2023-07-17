c2aa87c39860c09729c470c16b1acbbcc61743cd
mksiruguppa@gmail.com <mksiruguppa@gmail.com>
2023-07-17 19:09:09 -0400

    Add project files.

---
+++ Mounika_NUnitProject/Mounika_NUnitProject/UnitTest/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mounika_NUnitProject.UnitTest
+{
+    public class Calculator
+    {
+        public int AddTwoNumbers(int a, int b)
+        { 
+            return a + b; 
+        }
+        public int MultiplyTwoNumbers(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ Mounika_NUnitProject/Mounika_NUnitProject/UnitTest/CalculatorTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mounika_NUnitProject.UnitTest
+{
+    public class CalculatorTests
+    {
+        [Test]
+        public void ValidateMyCalculatorAddFunctionality()
+        {
+            int a = 3;
+            int b = 5;
+            int expectedResult = 8;
+
+            Calculator calculator = new Calculator();
+            int actualResult = calculator.AddTwoNumbers(a, b);
+            if (actualResult == expectedResult)
+            {
+                Console.WriteLine("oh god ! i know sum");
+            }
+            else
+            {
+                Console.WriteLine("oh god ! i dont know sum");
+            }
+        }
+        [Test]
+        public void ValidateMyCalculatorMultiplicationFunctionality()
+        {
+            int a = 3;
+            int b = 5;
+            int expectedResult = 15;
+
+            Calculator calculator = new Calculator();
+            int actualResult = calculator.MultiplyTwoNumbers(a, b);
+            if (actualResult == expectedResult)
+            {
+                Console.WriteLine("oh god ! i know Multiplication");
+            }
+            else
+            {
+                Console.WriteLine("oh god ! i dont know Multiplication");
+            }
+
+        }
+    }
+}
